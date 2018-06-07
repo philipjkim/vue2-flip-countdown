6be8f2f23a29e405d91a85b7ebdea8272ce0b0d7
PJ Kim <pj.kim@kakaocorp.com>
2018-06-07 19:34:02 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# vue-flip-countdown
+
+A simple flip countdown timer component for Vue.js 2.x
+
+## Installation
+
+```
+# not working yet
+npm i vue-flip-countdown --save
+```
+
+## Usage
+
+```vue
+<template>
+  <div>
+    <flip-countdown deadline="2018-12-25 00:00:00"></flip-countdown>
+  </div>
+</template>
+
+<script>
+  import FlipCountdown from 'vue-flip-countdown'
+
+  export default {
+      components: { FlipCountdown }
+  }
+</script>
+```
+
+# References
+
+- [vuejs-countdown](https://github.com/getanwar/vuejs-countdown)
+- [Demo for 'Flip clock & countdown, Vue'](https://codepen.io/shshaw/pen/BzObXp)
---
+++ src/FlipCountdown.vue
@@ -0,0 +1,300 @@
+<template>
+    <div class="container flip-clock">
+        <template v-for="data in timeData" v-show="show">
+            <span v-bind:key="data.label" class="flip-clock__piece" :id="data.elementId">
+                <span class="flip-clock__card flip-card">
+                <b class="flip-card__top">{{data.current | twoDigits}}</b>
+                <b class="flip-card__bottom" v-bind:data-value="data.current | twoDigits"></b>
+                <b class="flip-card__back" v-bind:data-value="data.previous | twoDigits"></b>
+                <b class="flip-card__back-bottom" v-bind:data-value="data.previous | twoDigits"></b>
+                </span>
+                <span class="flip-clock__slot">{{data.label}}</span>
+            </span>
+        </template>
+    </div>
+</template>
+
+<script>
+let interval = null
+export default {
+  props: {
+    deadline: {
+      type: String
+    },
+    stop: {
+      type: Boolean
+    }
+  },
+  data () {
+    return {
+      now: Math.trunc(new Date().getTime() / 1000),
+      date: null,
+      diff: 0,
+      show: false,
+      timeData: [
+        {
+          current: 0,
+          previous: 0,
+          label: 'Days',
+          elementId: 'days'
+        },
+        {
+          current: 0,
+          previous: 0,
+          label: 'Hours',
+          elementId: 'hours'
+        },
+        {
+          current: 0,
+          previous: 0,
+          label: 'Minutes',
+          elementId: 'minutes'
+        },
+        {
+          current: 0,
+          previous: 0,
+          label: 'Seconds',
+          elementId: 'seconds'
+        }
+      ]
+    }
+  },
+  created () {
+    if (!this.deadline) {
+      throw new Error("Missing props 'deadline'")
+    }
+    const endTime = this.deadline
+    this.date = Math.trunc(Date.parse(endTime.replace(/-/g, '/')) / 1000)
+    if (!this.date) {
+      throw new Error("Invalid props value, correct the 'deadline'")
+    }
+    interval = setInterval(() => {
+      this.now = Math.trunc(new Date().getTime() / 1000)
+    }, 1000)
+  },
+  mounted () {
+    if (this.diff !== 0) {
+      this.show = true
+    }
+  },
+  computed: {
+    seconds () {
+      return Math.trunc(this.diff) % 60
+    },
+    minutes () {
+      return Math.trunc(this.diff / 60) % 60
+    },
+    hours () {
+      return Math.trunc(this.diff / 60 / 60) % 24
+    },
+    days () {
+      return Math.trunc(this.diff / 60 / 60 / 24)
+    }
+  },
+  watch: {
+    now (value) {
+      this.diff = this.date - this.now
+      if (this.diff <= 0 || this.stop) {
+        this.diff = 0
+        clearInterval(interval)
+      } else {
+        this.updateTime(0, this.days)
+        this.updateTime(1, this.hours)
+        this.updateTime(2, this.minutes)
+        this.updateTime(3, this.seconds)
+      }
+    }
+  },
+  filters: {
+    twoDigits (value) {
+      if (value.toString().length <= 1) {
+        return '0' + value.toString()
+      }
+      return value.toString()
+    }
+  },
+  methods: {
+    updateTime (idx, newValue) {
+      if (idx >= this.timeData.length || newValue === undefined) {
+        return
+      }
+
+      if (window['requestAnimationFrame']) {
+        this.frame = requestAnimationFrame(this.updateTime.bind(this))
+      }
+
+      const d = this.timeData[idx]
+      const val = (newValue < 0 ? 0 : newValue)
+
+      if (val !== d.current) {
+        d.previous = d.current
+        d.current = val
+
+        const el = document.querySelector(`#${d.elementId}`)
+        if (el) {
+          el.classList.remove('flip')
+          void el.offsetWidth
+          el.classList.add('flip')
+        }
+      }
+    }
+  },
+  beforeDestroy () {
+    if (window['cancelAnimationFrame']) {
+      cancelAnimationFrame(this.frame)
+    }
+  },
+  destroyed () {
+    clearInterval(interval)
+  }
+}
+</script>
+
+<style scoped lang="less">
+.flip-clock {
+  text-align: center;
+  perspective: 600px;
+  margin: 0 auto;
+
+  *,
+  *:before,
+  *:after { box-sizing: border-box; }
+}
+
+
+.flip-clock__piece {
+  display: inline-block;
+  margin: 0 0.2vw;
+  
+  @media (min-width: 1000px) {
+    margin: 0 5px;
+  }
+}
+
+.flip-clock__slot {
+  font-size: 1rem;
+  line-height: 1.5;
+  display: block;
+}
+
+@halfHeight: 0.72em;
+@borderRadius: 0.15em;
+
+.flip-card {
+  display: block;
+  position: relative;
+  padding-bottom: @halfHeight;
+  font-size: 2.25rem;
+  line-height: 0.95;
+}
+
+@media (min-width: 1000px) {
+  .flip-clock__slot { font-size: 1.2rem; }
+  .flip-card { font-size: 3rem; }
+}
+
+.flip-card__top,
+.flip-card__bottom,
+.flip-card__back-bottom,
+.flip-card__back::before,
+.flip-card__back::after {
+  display: block;
+  height: @halfHeight;
+  color: #cca900;
+  background: #222;
+  padding: 0.23em 0.25em 0.4em;
+  border-radius: @borderRadius @borderRadius 0 0;
+  backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
+  transform-style: preserve-3d;
+  width: 1.7em;
+  height: @halfHeight;
+}
+
+.flip-card__bottom,
+.flip-card__back-bottom {
+  color: #ffdc00;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  border-top: solid 1px #000;
+  background: #393939;
+  border-radius: 0 0 @borderRadius @borderRadius;
+  pointer-events: none;
+  overflow: hidden;
+  z-index: 2;
+}
+
+.flip-card__back-bottom {
+  z-index: 1;
+}
+
+.flip-card__bottom::after,
+.flip-card__back-bottom::after {
+  display: block;
+  margin-top: -@halfHeight;
+}
+
+.flip-card__back::before,
+.flip-card__bottom::after,
+.flip-card__back-bottom::after {
+  content: attr(data-value);
+}
+
+.flip-card__back {
+  position: absolute;
+  top: 0;
+  height: 100%;
+  left: 0%;
+  pointer-events: none;
+}
+
+.flip-card__back::before {
+  position: relative;
+  overflow: hidden;
+  z-index: -1;
+}
+
+.flip .flip-card__back::before {
+  z-index: 1;
+  animation: flipTop 0.3s cubic-bezier(.37,.01,.94,.35);
+  animation-fill-mode: both;
+  transform-origin: center bottom;
+}
+
+.flip .flip-card__bottom {
+  transform-origin: center top;
+  animation-fill-mode: both;
+  animation: flipBottom 0.6s cubic-bezier(.15,.45,.28,1);
+}
+
+@keyframes flipTop {
+  0% {
+    transform: rotateX(0deg);
+    z-index: 2;
+  }
+  0%, 99% {
+    opacity: 1;
+  }
+  100% {
+    transform: rotateX(-90deg);
+    opacity: 0;
+  }
+}
+
+@keyframes flipBottom {
+  0%, 50% {
+    z-index: -1;
+    transform: rotateX(90deg);
+    opacity: 0;
+  }
+  51% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+    transform: rotateX(0deg);
+    z-index: 5;
+  }
+}
+</style>
