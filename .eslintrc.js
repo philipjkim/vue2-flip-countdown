c33e3b001e6721c2b2e2355e64842fd92e825865
PJ Kim <pj.kim@kakaocorp.com>
2018-06-08 10:11:53 +0900

    Add webpack and eslint configs and dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    env: {
+      browser: true,
+      node: true
+    },
+    extends: 'standard',
+    // required to lint *.vue files
+    plugins: [
+      'html'
+    ],
+    // add your custom rules here
+    rules: {},
+    globals: {}
+  }
+  
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// config for webpack v3
+
+const webpack = require('webpack')
+const path = require('path')
+const PROD = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: path.join(__dirname, '/src/FlipCountdown.vue'),
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'vue-flip-countdown.js',
+
+    libraryTarget: 'umd',
+    library: 'vue-flip-countdown',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            js: {
+              loader: 'babel-loader',
+              options: { presets: ['env'] }
+            },
+            less: 'vue-style-loader!css-loader!less-loader'
+          }
+        }
+      }
+    ]
+  },
+  plugins: PROD ? [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: !!PROD,
+      sourceMap: !PROD,
+      mangle: !!PROD,
+      compress: {
+        warnings: !PROD
+      }
+    })
+  ] : []
+}
